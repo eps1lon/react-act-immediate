1178cb95885751e7c86106a873f32a4e870338b1
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-03-31 19:57:00 +0200

    chore: Remove unused files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./styles.css";
-
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  return "See index.test.js";
 }
 
 const rootElement = document.getElementById("root");
