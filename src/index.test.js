4d61b67dc98dd8dd422a41d07b82e08a8031bded
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-03-31 19:59:54 +0200

    test: Fix act warning

--- src/index.test.js
+++ src/index.test.js
@@ -17,11 +17,15 @@ beforeAll(() => {
   configure({ adapter: new Adapter() });
 });
 
-test("SimpleComponent", done => {
+jest.useFakeTimers();
+
+test("SimpleComponent", () => {
   const wrapper = mount(<SimpleComponent />);
 
-  jest.runAllImmediates();
-  console.log(wrapper.text());
+  expect(wrapper.text()).toBe("initial");
 
-  setImmediate(() => done());
+  act(() => {
+    jest.runAllImmediates();
+  });
+  expect(wrapper.text()).toBe("updated");
 });
